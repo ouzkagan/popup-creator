71dee18aaec2d18a5f52e16b2c5009d2ef68543d
oğuz kağan <36485087+ouzkagan@users.noreply.github.com>
2022-10-03 17:31:36 +0300

    storybook working together with scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,58 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    // 'storybook-addon-next',
+    // '@storybook/addon-a11y',
   ],
+
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  // webpackFinal: (config) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve?.alias,
+  //     '@': [path.resolve(__dirname, '../src/'), path.resolve(__dirname, '../')],
+  //   };
+  //   config.resolve.roots = [
+  //     path.resolve(__dirname, '../public'),
+  //     'node_modules',
+  //   ];
+  //   return config;
+  // },
+  // webpackFinal: async (config) => {
+  //   return {
+  //     ...config,
+  //     config.resolve.modules = [
+  //       ...(config.resolve.modules || []),
+  //       path.resolve('../')
+  //     ]
+
+  //   };
+  // },
+  // webpackFinal: async (config) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '~/': path.resolve(__dirname, '../src/'),
+  //   };
+  //   config.resolve.extensions.push('.ts', '.tsx');
+  //   return config;
+  // },
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
   },
 };
